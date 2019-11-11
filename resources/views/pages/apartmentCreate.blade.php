58f35f70f280135d0b402b00ba63f11ded223488
Walter Satriano <walter.satri@gmail.com>
2019-11-11 17:24:40 +0100

    lievi modifiche create ed edit

--- resources/views/pages/apartmentCreate.blade.php
+++ resources/views/pages/apartmentCreate.blade.php
@@ -2,100 +2,88 @@
 
 @section('content')
 
- <div class="container apt_create_box">
-
+  <div class="container apt_create_box">
     <a class="button_style" href="{{ route('home.index') }}">HOME</a>
 
-   <h3>Aggiungi un nuovo appartamento</h3>
-
-
+    <h3>Aggiungi un nuovo appartamento</h3>
 
     <div class="add_apartment">
       <form action="{{ route('apartment.store') }}" method="post"
-      accept-charset="UTF-8"
-      enctype="multipart/form-data">
+        accept-charset="UTF-8"
+        enctype="multipart/form-data">
 
-      @csrf
-      @method('POST')
+        @csrf
+        @method('POST')
 
-      <div class="form-group">
-        <label for="title">Titolo</label>
-        <input type="text" name="title" class="form-control" placeholder="" value="">
-      </div>
-
-      <div class="form-group">
-        <label for="desc">Descrizione</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1"  name="desc" rows="5"  placeholder="inserisci descrizione"></textarea>
-      </div>
-
-      <div class="number_label">
-
-        <div class="form-row">
-
-          <div class="form-group col-md-3">
-            <label for="rooms">Numero stanze</label>
-            <input type="number" class="form-control" name="rooms" value="">
-          </div>
-          <div class="form-group col-md-3">
-            <label  for="beds">Numero letti</label>
-            <input type="number" class="form-control" name="beds" value="">
-          </div>
-          <div class="form-group col-md-3">
-            <label for="toilettes">Numero Bagni</label>
-            <input type="number" class="form-control" name="toilettes" value="">
-          </div>
-          <div class="form-group col-md-3">
-            <label for="square_meters">Metri quadrati</label>
-            <input type="number" class="form-control" name="square_meters" value="">
-          </div>
-
+        <div class="form-group">
+          <label for="title">Titolo</label>
+          <input type="text" name="title" class="form-control" placeholder="" value="">
         </div>
 
-      </div>
-
-      <div class="text_label">
-
-        <div class="form-row">
-
-          <div class="form-group col-md-3">
-            <label for="city">Città</label>
-            <input type="text" class="form-control" name="city" value="">
-          </div>
-          <div class="form-group col-md-2">
-            <label for="prov">Provincia</label>
-            <input type="text" class="form-control" name="prov" value="">
-          </div>
-          <div class="form-group col-md-2">
-            <label for="cap">CAP</label>
-            <input type="text" class="form-control" name="cap" value="">
-          </div>
-          <div class="form-group col-md-5">
-            <label for="address">Indirizzo</label>
-            <input type="text" class="form-control" name="address" value="">
-          </div>
-
+        <div class="form-group">
+          <label for="desc">Descrizione</label>
+          <textarea class="form-control" name="desc" rows="5"  placeholder="inserisci descrizione"></textarea>
         </div>
 
-       <div class="immagini_margin">
-         <label for="img">Immagine</label> <br>
-         <input class="input_img_apt" type="file" name="img" accept="image/*">
-       </div>
+        <div class="number_label">
+          <div class="form-row">
 
+            <div class="form-group col-md-3">
+              <label for="rooms">Numero stanze</label>
+              <input type="number" class="form-control" name="rooms" value="">
+            </div>
 
+            <div class="form-group col-md-3">
+              <label  for="beds">Numero letti</label>
+              <input type="number" class="form-control" name="beds" value="">
+            </div>
 
-      @foreach ($services as $service)
-        {{-- <label for="{{ $service -> service_category }}">{{ $service -> service_category }}</label>
-        <input type="checkbox" name="{{ $service -> service_category }}[]" value=""> --}}
-        <input type="checkbox" name="services[]" value="{{ $service -> id }}"> {{ $service -> service_category }}<br/><br/>
-      @endforeach
+            <div class="form-group col-md-3">
+              <label for="toilettes">Numero Bagni</label>
+              <input type="number" class="form-control" name="toilettes" value="">
+            </div>
 
+            <div class="form-group col-md-3">
+              <label for="square_meters">Metri quadrati</label>
+              <input type="number" class="form-control" name="square_meters" value="">
+            </div>
+          </div>
+        </div>
 
-      <button class="button_style" type="submit" name="button">CREA</button>
+        <div class="text_label">
+          <div class="form-row">
 
-    </form>
+            <div class="form-group col-md-3">
+              <label for="city">Città</label>
+              <input type="text" class="form-control" name="city" value="">
+            </div>
+            <div class="form-group col-md-2">
+              <label for="prov">Provincia</label>
+              <input type="text" class="form-control" name="prov" value="">
+            </div>
+            <div class="form-group col-md-2">
+              <label for="cap">CAP</label>
+              <input type="text" class="form-control" name="cap" value="">
+            </div>
+            <div class="form-group col-md-5">
+              <label for="address">Indirizzo</label>
+              <input type="text" class="form-control" name="address" value="">
+            </div>
+          </div>
+
+          <div class="immagini_margin">
+            <label for="img">Immagine</label> <br>
+            <input class="input_img_apt" type="file" name="img" accept="image/*">
+          </div>
+
+          @foreach ($services as $service)
+            <input type="checkbox" name="services[]" value="{{ $service -> id }}"> {{ $service -> service_category }}<br/><br/>
+          @endforeach
+
+          <button class="button_style" type="submit" name="button">CREA</button>
+        </div>
+      </form>
+    </div>
   </div>
-
-  </div>
-
 
 @endsection
--- resources/views/pages/apartmentEdit.blade.php
+++ resources/views/pages/apartmentEdit.blade.php
@@ -23,31 +23,32 @@
 
         <div class="form-group">
           <label for="desc">Descrizione</label>
-          <textarea class="form-control" id="exampleFormControlTextarea1"  name="desc" rows="5"  placeholder="inserisci descrizione" value="{{ $apartment -> desc }}"></textarea>
+          <textarea class="form-control" name="desc" rows="5"> {{ $apartment -> desc }} </textarea>
         </div>
 
-        <div class="number_label">
+        {{-- id="exampleFormControlTextarea1" --}}
 
+        <div class="number_label">
           <div class="form-row">
 
             <div class="form-group col-md-3">
               <label for="rooms">Numero stanze</label>
-              <input type="text" name="rooms" class="form-control" placeholder="" value="{{ $apartment -> rooms }}">
+              <input type="number" name="rooms" class="form-control" placeholder="" value="{{ $apartment -> rooms }}">
             </div>
 
             <div class="form-group col-md-3">
               <label for="beds">Numero letti</label>
-              <input type="text" name="beds" class="form-control" placeholder="" value="{{ $apartment -> beds }}">
+              <input type="number" name="beds" class="form-control" placeholder="" value="{{ $apartment -> beds }}">
             </div>
 
             <div class="form-group col-md-3">
               <label for="toilettes">Numero Bagni</label>
-              <input type="text" name="toilettes" class="form-control" placeholder="" value="{{ $apartment -> toilettes }}">
+              <input type="number" name="toilettes" class="form-control" placeholder="" value="{{ $apartment -> toilettes }}">
             </div>
 
             <div class="form-group col-md-3">
               <label for="square_meters">Metri quadrati</label>
-              <input type="text" name="square_meters" class="form-control" placeholder="" value="{{ $apartment -> square_meters }}">
+              <input type="number" name="square_meters" class="form-control" placeholder="" value="{{ $apartment -> square_meters }}">
             </div>
           </div>
 
@@ -84,11 +85,21 @@
           <input class="input_img_apt" type="file" name="img" accept="image/*"> <br>
         </div>
 
-
-
-        @foreach ($services as $service)
-
-          <input type="checkbox" name="services[]" value="{{ $service -> id }}" > {{ $service -> service_category }}<br/><br/>
+        {{-- foreach per vedere i servizi già fleggati --}}
+        @foreach ( $services as $service )
+          @php
+          $array = ($apartment -> services)->pluck('id')->toArray(); // ritorna un array da una collection e prende gli id della colonna
+          @endphp
+          {{-- checkbox per servizi aggiuntivi --}}
+          <div>
+            <div>
+              <label>
+                <input type="checkbox" name="services[]" value="{{ $service -> id }}"
+                {{ (in_array($service -> id, old('service', $array )) ) ? 'checked' : ''}}>
+                {{ $service -> service_category }}
+              </label>
+            </div>
+          </div>
         @endforeach
 
         <button class="button_style" type="submit" name="button">Update</button>
